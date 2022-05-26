2324b6e9b5aa11ccb15b7604d048a89edcdda005
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2022-05-26 15:29:27 +0900

    feat: 자동 응답 로직 구현

--- main.js
+++ main.js
@@ -24,14 +24,17 @@ inboxText1.addEventListener('click', function () {
     textArea.style.display = "none";
     recommendationArea.style.display = "block";
     recommendationTargetMessage.innerText = inbotText1P.innerText;
+    recommendationText1P.innerText = "부르셨나요?"
+    recommendationText2P.innerText = "말씀하세요"
 })
 
 inboxText2.addEventListener('click', function () {
     textArea.style.display = "none";
     recommendationArea.style.display = "block";
     recommendationTargetMessage.innerText = inbotText2P.innerText;
+    recommendationText1P.innerText = "오키오키~"
+    recommendationText2P.innerText = "안될 것 같습니다"
 })
-
 
 let recommendationTouchStart = 0;
 let recommendationTouchEnd = 0;
@@ -53,22 +56,6 @@ recommendationArea.addEventListener("touchmove", (e) => {
     }
 })
 
-let autoAnswerTouchStart = 0;
-let autoAnswerTouchEnd = 0;
-autoAnswerArea.addEventListener("touchstart", (e) => {
-    console.log(e);
-    autoAnswerTouchStart = e.touches[0].pageY;
-})
-
-autoAnswerArea.addEventListener("touchmove", (e) => {
-    autoAnswerTouchEnd = e.touches[0].pageY;
-    // Swipe Down
-    if (autoAnswerTouchEnd - autoAnswerTouchStart >= 150) {
-        autoAnswerArea.style.display = "none";
-        recommendationArea.style.display = "block";
-    }
-})
-
 recommendationText1.addEventListener('click', function () {
     recommendationArea.style.display = "none";
     finalAnswerArea.style.display = "block";
@@ -81,7 +68,81 @@ recommendationText2.addEventListener('click', function () {
     outboxTextP.innerText = recommendationText2P.innerText;
 })
 
+let autoAnswerTouchStart = 0;
+let autoAnswerTouchEnd = 0;
+autoAnswerArea.addEventListener("touchstart", (e) => {
+    autoAnswerTouchStart = e.touches[0].pageY;
+})
 
+autoAnswerArea.addEventListener("touchmove", (e) => {
+    autoAnswerTouchEnd = e.touches[0].pageY;
+    // Swipe Down
+    if (autoAnswerTouchEnd - autoAnswerTouchStart >= 150) {
+        autoAnswerArea.style.display = "none";
+        recommendationArea.style.display = "block";
+    }
+})
+
+let autoAnswerText1 = document.getElementById("auto-answer-text-1");
+let autoAnswerText2 = document.getElementById("auto-answer-text-2");
+let autoAnswerText3 = document.getElementById("auto-answer-text-3");
+
+let tuneToneText1P = document.getElementById("tune-tone-text-1-p");
+let tuneToneText2P = document.getElementById("tune-tone-text-2-p");
+let tuneToneText3P = document.getElementById("tune-tone-text-3-p");
+
+let tuneToneText1 = document.getElementById("tune-tone-text-1");
+let tuneToneText2 = document.getElementById("tune-tone-text-2");
+let tuneToneText3 = document.getElementById("tune-tone-text-3");
+
+autoAnswerText1.addEventListener("click", function() {
+    tuneToneText1P.innerText = "네 알겠습니다";
+    tuneToneText2P.innerText = "네";
+    tuneToneText3P.innerText = "오케이~!";
+    autoAnswerArea.style.display = "none";
+    tuneToneArea.style.display = "block";
+})
+
+autoAnswerText2.addEventListener("click", function () {
+    tuneToneText1P.innerText = "힘들 것 같습니다";
+    tuneToneText2P.innerText = "아니요";
+    tuneToneText3P.innerText = "힘들 듯!";
+    autoAnswerArea.style.display = "none";
+    tuneToneArea.style.display = "block";
+})
+
+autoAnswerText3.addEventListener("click", function () {
+    tuneToneText1P.innerText = "정말 감사합니다!";
+    tuneToneText2P.innerText = "감사합니다";
+    tuneToneText3P.innerText = "땡큐!";
+    autoAnswerArea.style.display = "none";
+    tuneToneArea.style.display = "block";
+})
+
+tuneToneText1.addEventListener("click", function() {
+    tuneToneArea.style.display = "none";
+    finalAnswerArea.style.display = "block";
+    outboxTextP.innerText = tuneToneText1P.innerText;
+})
+
+tuneToneText2.addEventListener("click", function () {
+    tuneToneArea.style.display = "none";
+    finalAnswerArea.style.display = "block";
+    outboxTextP.innerText = tuneToneText2P.innerText;
+})
+
+tuneToneText3.addEventListener("click", function () {
+    tuneToneArea.style.display = "none";
+    finalAnswerArea.style.display = "block";
+    outboxTextP.innerText = tuneToneText3P.innerText;
+})
+
+
+let backBtn = document.getElementById("back-btn");
+backBtn.addEventListener("click", function () {
+    tuneToneArea.style.display = "none";
+    autoAnswerArea.style.display = "block";
+})
 
 finalAnswerArea.addEventListener('click', function () {
     finalAnswerArea.style.display = "none";
